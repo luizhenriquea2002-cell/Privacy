4f5dd875ff62f6ec6465d596553dcfe799a92b25
luizhenriquea2002-cell <luizhenriquea2002@gmail.com>
2026-04-01 18:56:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+let posts = [];
+
+// criar post
+app.post("/post", (req, res) => {
+  posts.push(req.body);
+  res.json({ ok: true });
+});
+
+// listar posts
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.listen(3000, () => console.log("rodando"));
